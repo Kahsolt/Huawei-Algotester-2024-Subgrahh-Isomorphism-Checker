61b116e8eefdabace4388d61da0ad3a8c2471fb6
Kahsolt <kahsolt@qq.com>
2024-09-18 11:56:52 +0800

    add rx2py to benchemark, it is very slow though...

--- main_benchmark.py
+++ main_benchmark.py
@@ -2,17 +2,19 @@
 # Author: Armit
 # Create Time: 2024/09/17 
 
-# Copy & modify the impl. of networkx
-# https://networkx.org/documentation/stable/reference/algorithms/isomorphism.html
+# benchmark different implmentations
 
 from time import time
 from typing import List
-
 from tqdm import tqdm
-from rustworkx import PyGraph, graph_vf2_mapping
-
 from data import QUERY_GRAPH_EDGES, TARGET_GRAPHS_LEN, get_query_pair
+
+# ↓↓ rustworkx baseline
+from rustworkx import PyGraph, graph_vf2_mapping
+# ↓↓ networkx impl. migrated
 from main import Graph, find_isomorphism, Labels, Edges, Result
+# ↓↓ rustworkx impl. migrated
+from main_rustworkx_impl import StableGraph, Vf2Algorithm
 
 
 def make_graph(labels:Labels, edges:Edges, offset_by_one:bool=False) -> Graph:
@@ -35,8 +37,22 @@ def find_isomorphism_rx(g:PyGraph, s:PyGraph) -> Result:
   for it in vf2:
     return [e[0] for e in sorted(it.items(), key=lambda e: e[-1])]
 
+def to_rx2py(graph:Graph) -> StableGraph:
+  g = StableGraph(graph.n, graph.m)
+  node_ids: List[int] = []
+  for it in graph.labels:
+    node_ids.append(g.add_node(it))
+  for u, v in graph.edges:
+    g.add_edge(node_ids[u], node_ids[v])
+  return g
 
-def run_from_demo():
+def find_isomorphism_rx2py(g:StableGraph, s:StableGraph) -> Result:
+  mapping = Vf2Algorithm(g, s).next_vf2()
+  if mapping is not None:
+    return [e[0] for e in sorted(mapping.items(), key=lambda e: e[-1])]
+
+
+def run_demo():
   G  = make_graph(
     [4, 1, 3, 2, 1],
     [
@@ -80,12 +96,13 @@ def run_from_demo():
       print(f' {x + 1}', end='')
     print()
 
-def run_from_random(gid:int=12, n_edges:int=8, log:bool=False) -> bool:
+def run_random(gid:int=12, n_edges:int=8, log:bool=False) -> bool:
   if log: print(f'[Run] graph_id={gid}, subgraph_edges={n_edges}')
 
   G, S_list = get_query_pair(gid, n_edges)
   g = make_graph(*G)
   g_rx = to_rx(g)
+  g_rx2py = to_rx2py(g)
 
   ts_sum = 0.0
   results_nx = []
@@ -111,12 +128,25 @@ def run_from_random(gid:int=12, n_edges:int=8, log:bool=False) -> bool:
     if f: found_rx.append(i)
   if log: print(f'>> [Rustworkx] time cost: {ts_sum:.3f}')
 
-  chk = found_nx == found_rx
+  ts_sum = 0.0
+  results_rx2py = []
+  found_rx2py = []
+  for i, S in enumerate(S_list, start=1):
+    s_rx2py = to_rx2py(make_graph(*S))
+    ts_start = time()
+    f = find_isomorphism_rx2py(g_rx2py, s_rx2py)
+    ts_sum += time() - ts_start
+    results_rx2py.append(f)
+    if f: found_rx2py.append(i) 
+  if log: print(f'>> [RustworkxImpl] time cost: {ts_sum:.3f}')
+
+  chk = found_nx == found_rx == found_rx2py
 
   if log and not chk:
     print('>> check FAILED! :/')
     print(f'   found_nx ({len(found_nx)}):', found_nx)
     print(f'   found_rx ({len(found_rx)}):', found_rx)
+    print(f'   found_rx2py ({len(found_rx2py)}):', found_rx2py)
     print()
 
   return chk
@@ -131,6 +161,6 @@ if __name__ == '__main__':
   for gid in range(n_lim):
     # 跑各种边数的模式子图，各1000个模式
     for n_edges in QUERY_GRAPH_EDGES:
-      ok += run_from_random(gid, n_edges, log=False)
+      ok += run_random(gid, n_edges, log=False)
       pbar.update()
       pbar.set_postfix({'ok': ok, 'sr': ok / pbar.n})
--- main_rustworkx_impl.py
+++ main_rustworkx_impl.py
@@ -2,14 +2,11 @@
 # Author: Armit
 # Create Time: 2024/09/18 
 
-# Copy & modify the impl. of rustworkx
-# https://www.rustworkx.org/apiref/rustworkx.graph_vf2_mapping.html#rustworkx.graph_vf2_mapping
-
-''' ↓↓↓ Copy from https://github.com/6god-rail-flower-water/Subgraph-Isomorphic '''
+''' ↓↓↓ Copy & modified from https://github.com/6god-rail-flower-water/Subgraph-Isomorphic '''
 
 from enum import Enum
-from collections import defaultdict
-from typing import Iterator, List
+from typing import Iterator, List, Tuple
+from dataclasses import dataclass
 import numpy as np
 
 INDEX_MAX = 5000
@@ -19,16 +16,16 @@ class OpenList(Enum):
     In = 1
     Other = 2
 
-class FrameEnum(Enum):
+class FrameType(Enum):
     Outer = "Outer"
     Inner = "Inner"
     Unwind = "Unwind"
 
+@dataclass
 class FrameData:
-    def __init__(self, frame_type:FrameEnum, nodes:List[int]=None, open_list:OpenList=None):
-        self.frame_type = frame_type
-        self.nodes = nodes
-        self.open_list = open_list
+    frame_type: FrameType
+    nodes: List[int] = None
+    open_list: OpenList = None
 
 class StableGraph:
     def __init__(self, num_nodes:int, num_edges:int):
@@ -64,59 +61,51 @@ class StableGraph:
 
     def node_weight(self, index:int) -> int:
         return self.nodes.get(index)
-    
-    def get_edge(self, index:int) -> List[int]:
-        return self.edges.get(index)
 
     def edge_references(self) -> Iterator[List[int]]: 
         for edge_index, (source, target) in self.edges.items():
             yield source, target
-    
-    def collect_nodes(self) -> np.ndarray:
-        return np.array(list(self.nodes.keys()))
-    
-    def get_degrees(self) -> np.ndarray:
-        degree = np.zeros(self.num_nodes, dtype=int)
+
+    def get_degrees(self) -> List[int]:
+        degree = [0] * self.num_nodes
         for edge in self.edges.values():
             degree[edge[0]] += 1
             degree[edge[1]] += 1
         return degree
-    
+
     def get_neighbours(self, index:int) -> Iterator[int]:
         for source, target in self.edge_references():
-            if source == index:
-                yield target
-            elif target == index:
-                yield source
+            if   source == index: yield target
+            elif target == index: yield source
 
-def find_max_element(vd:np.ndarray, i:int, conn_in:np.ndarray, dout:np.ndarray, conn_out:np.ndarray, din:np.ndarray) -> List[int]:
+def find_max_element(vd:List[int], i:int, conn_in:List[int], dout:List[int], conn_out:List[int], din:List[int]) -> List[int]:
     def sorting_key(node):
         return (conn_in[node], dout[node], conn_out[node], din[node], -node)
     sub_vd = vd[i:]
     max_index, max_item = max(enumerate(sub_vd), key=lambda x: sorting_key(x[1]))
     return i + max_index, max_item
 
-def adj_matrix(G:StableGraph) -> dict:
-    matrix = defaultdict(int)
+def adj_mat(G:StableGraph) -> set:
+    adj = set()
     for edge in G.edge_references():
         source, target = min(edge[0], edge[1]), max(edge[0], edge[1])
-        matrix[(source, target)] += 1
-    return matrix
+        adj.add((source, target))
+    return adj
 
-def edge_multiplicity(matrix:dict, a:int, b:int) -> int:
-    item = (min(a, b), max(a, b))
-    return matrix.get(item, 0)
+def has_edge(adj:set, a:int, b:int) -> int:
+    return int((min(a, b), max(a, b)) in adj)
 
 class VF2ppSorter:
     def __init__(self, G:StableGraph):
         self.G = G
 
-    def sort(self) -> np.ndarray:
+    def sort(self) -> List[int]:
         n = self.G.node_bound()
         dout = self.G.get_degrees()
-        din = np.zeros(n, dtype=int)
-        conn_in, conn_out = np.zeros(n), np.zeros(n)
-        order = []
+        din = [0] * n
+        conn_in = [0] * n
+        conn_out = [0] * n
+        order: List[int] = []
 
         def process(vd:list) -> list:
             for i in range(len(vd)):
@@ -125,48 +114,39 @@ class VF2ppSorter:
                 order.append(item)
                 for neigh in self.G.get_neighbours(item):
                     conn_in[neigh] += 1
-            
             return vd
-        
-        seen = np.full(n, False, dtype=bool)
+
+        seen = np.full(n, False, dtype=bool)    # BUG: here must use ndarray rather than List[bool], why???
         def bfs_tree(root:int):
-            if seen[root] is True:
-                return
-            
+            if seen[root] is True: return
+
             next_level = []
             seen[root] = True
             next_level.append(root)
-            
-            while len(next_level) != 0:
-                this_level = next_level
+
+            while len(next_level):
                 this_level = process(next_level)
-                
                 next_level = []
                 for bfs_node in this_level:
                     for neighbor in self.G.get_neighbours(bfs_node):
                         if seen[neighbor] is False:
                             seen[neighbor] = True
                             next_level.append(neighbor)
-                            
-        sorted_nodes = self.G.collect_nodes()
-        sorted_nodes_list = sorted_nodes.tolist()
-        sorted_list = sorted(sorted_nodes_list, key=lambda x: (dout[x], din[x], -x))
-        sorted_list.reverse()
 
+        sorted_list = list(self.G.nodes)
+        sorted_list.sort(key=lambda x: (dout[x], din[x], -x), reverse=True)
         for node in sorted_list:
             bfs_tree(node)
-        return np.asarray(order)
+        return order
 
-    def reorder(self) -> tuple[StableGraph, dict]:
+    def reorder(self) -> Tuple[StableGraph, dict]:
         order_nodes = self.sort()
         new_graph = StableGraph(self.G.num_nodes, self.G.num_edges)
         id_map = {}
-
         for node_index in order_nodes:
             node_data = self.G.node_weight(node_index)
             new_index = new_graph.add_node(node_data)
             id_map[node_index] = new_index
-
         for source, target in self.G.edge_references():
             new_graph.add_edge(id_map[source], id_map[target])
         return new_graph, {v: k for k, v in id_map.items()}
@@ -175,48 +155,48 @@ class Vf2State:
     def __init__(self, G:StableGraph):
         self.G = G
         self.c0 = self.G.num_nodes
-        self.mapping = np.full(self.c0, INDEX_MAX, dtype=int)
-        self.out = np.full(self.c0, 0, dtype=int)
-        self.ins = np.full(0, 0, dtype=int)
+        self.mapping = [INDEX_MAX] * self.c0
+        self.out = [0] * self.c0
+        self.ins = []       # wft, empty?
         self.out_size = 0
         self.ins_size = 0
-        self.adjacency_matrix = adj_matrix(G)
+        self.adjacency_matrix = adj_mat(G)
         self.generation = 0
 
     def is_complete(self) -> bool:
-        return self.generation == self.mapping.size
-    
+        return self.generation == len(self.mapping)
+
     def push_mapping(self, idx_from:int, idx_to:int):
         self.generation += 1
         s = self.generation
         self.mapping[idx_from] = idx_to
-        
+
         for ix in self.G.get_neighbours(idx_from):
             if self.out[ix] == 0:
                 self.out[ix] = s
                 self.out_size += 1
-                
+
     def pop_mapping(self, idx_from:int):
         s = self.generation
         self.generation -= 1
         self.mapping[idx_from] = INDEX_MAX
-        
+
         for ix in self.G.get_neighbours(idx_from):
             if self.out[ix] == s:
                 self.out[ix] = 0
                 self.out_size -= 1
-                
-    def next_out_index(self, idx_from:int) -> object:
+
+    def next_out_index(self, idx_from:int) -> int:
         for ix, elt in enumerate(self.out[idx_from:]):
             if elt > 0 and self.mapping[idx_from + ix] == INDEX_MAX:
                 return ix
 
-    def next_in_index(self, idx_from:int) -> object:
+    def next_in_index(self, idx_from:int) -> int:
         for ix, elt in enumerate(self.ins[idx_from:]):
             if elt > 0 and self.mapping[idx_from + ix] == INDEX_MAX:
                 return ix
     
-    def next_rest_idx(self, idx_from:int) -> object:
+    def next_rest_index(self, idx_from:int) -> int:
         for ix, elt in enumerate(self.mapping[idx_from:]):
             if elt == INDEX_MAX:
                 return ix
@@ -231,7 +211,7 @@ class Vf2Algorithm:
         self.g1, self.node_map_g1 = VF2ppSorter(g1).reorder()
 
         self.st = [Vf2State(g0), Vf2State(g1)]
-        self.stack: List[FrameData] = [FrameData(FrameEnum.Outer)]
+        self.stack: List[FrameData] = [FrameData(FrameType.Outer)]
 
     def mapping(self) -> dict:
         mapping = {}
@@ -240,7 +220,7 @@ class Vf2Algorithm:
             g1_node = self.node_map_g1[index]
             mapping[g0_node] = g1_node
         return mapping
-    
+
     def next_candidate(self) -> tuple[int, int, OpenList]:
         to_idx = self.st[1].next_out_index(idx_from=0)
         from_idx = None
@@ -257,15 +237,15 @@ class Vf2Algorithm:
                 open_list = OpenList.In
                 
         if to_idx is None or from_idx is None:
-            to_idx = self.st[1].next_rest_idx(idx_from=0)
+            to_idx = self.st[1].next_rest_index(idx_from=0)
             if to_idx != None:
-                from_idx = self.st[0].next_rest_idx(idx_from=0)
+                from_idx = self.st[0].next_rest_index(idx_from=0)
                 open_list = OpenList.Other
                 
         if to_idx != None and from_idx != None:
             return from_idx, to_idx, open_list
         return [None, None, None]
-    
+
     def next_from_ix(self, nx:int, open_list:OpenList):
         start = nx + 1
         if open_list == OpenList.Out:
@@ -273,7 +253,7 @@ class Vf2Algorithm:
         elif open_list == OpenList.In:
             cand0 = self.st[0].next_in_index(idx_from=start)
         else:
-            cand0 = self.st[0].next_rest_idx(idx_from=start)
+            cand0 = self.st[0].next_rest_index(idx_from=start)
         if cand0 is None: return
 
         cand0 += start
@@ -293,17 +273,16 @@ class Vf2Algorithm:
         for j in range(2):
             for n_neigh in self.st[j].G.get_neighbours(nodes[j]):
                 succ_count[j] += 1
-                if j == 0:
-                    continue 
+                if j == 0: continue
+
                 if nodes[j] != n_neigh:
                     m_neigh = self.st[j].mapping[n_neigh]
                 else:
                     m_neigh = nodes[1 - j]
-                if m_neigh == INDEX_MAX:
-                    continue
+                if m_neigh == INDEX_MAX: continue
 
                 # A strange expression in source code 
-                if edge_multiplicity(self.st[1-j].adjacency_matrix, m_neigh, nodes[1-j]) < edge_multiplicity(self.st[j].adjacency_matrix, nodes[j], n_neigh):
+                if has_edge(self.st[1-j].adjacency_matrix, m_neigh, nodes[1-j]) < has_edge(self.st[j].adjacency_matrix, nodes[j], n_neigh):
                     return False
 
         if succ_count[0] < succ_count[1]:
@@ -322,14 +301,14 @@ class Vf2Algorithm:
             return False
 
         return True
-    
+
     def next_vf2(self):
         if self.st[0].G.num_nodes < self.st[1].G.num_nodes or self.st[0].G.num_edges < self.st[1].G.num_edges:
             return
 
         while len(self.stack) > 0:
             frame = self.stack.pop()
-            if frame.frame_type == FrameEnum.Unwind:
+            if frame.frame_type == FrameType.Unwind:
                 nodes = frame.nodes
                 ol = frame.open_list
                 self.pop_state(nodes)
@@ -338,29 +317,29 @@ class Vf2Algorithm:
                     continue
                 else:
                     nx = self.next_from_ix(nodes[0], ol)
-                    f = FrameData(FrameEnum.Inner, [nx, nodes[1]], ol)
+                    f = FrameData(FrameType.Inner, [nx, nodes[1]], ol)
                     self.stack.append(f)
                     
-            if frame.frame_type == FrameEnum.Outer:
+            if frame.frame_type == FrameType.Outer:
                 if self.next_candidate() == [None, None, None]:
                     if self.st[1].is_complete():
                         return self.mapping()
                     continue
                 else:
                     nx, mx, ol = self.next_candidate()
-                    f = FrameData(FrameEnum.Inner, [nx, mx], ol)
+                    f = FrameData(FrameType.Inner, [nx, mx], ol)
                     self.stack.append(f)
                     
-            if frame.frame_type == FrameEnum.Inner:
+            if frame.frame_type == FrameType.Inner:
                 nodes = frame.nodes
                 ol = frame.open_list
                 
                 if self.is_feasible(nodes):
                     self.push_state(nodes)
                     if self.st[0].out_size >= self.st[1].out_size and self.st[0].ins_size >= self.st[1].ins_size:
-                        f0 = FrameData(FrameEnum.Unwind, nodes, ol)
+                        f0 = FrameData(FrameType.Unwind, nodes, ol)
                         self.stack.append(f0)
-                        self.stack.append(FrameData(FrameEnum.Outer))
+                        self.stack.append(FrameData(FrameType.Outer))
                         continue
 
                     self.pop_state(nodes)
@@ -369,12 +348,7 @@ class Vf2Algorithm:
                     continue
                 else:
                     nx = self.next_from_ix(nodes[0], ol)
-                    f = FrameData(FrameEnum.Inner, [nx, nodes[1]], ol)
+                    f = FrameData(FrameType.Inner, [nx, nodes[1]], ol)
                     self.stack.append(f)
 
-''' ↑↑↑ Copy from https://github.com/6god-rail-flower-water/Subgraph-Isomorphic '''
-
-
-if __name__ == '__main__':
-    #run_from_stdin()
-    run_from_random()
+''' ↑↑↑ Copy & modified from https://github.com/6god-rail-flower-water/Subgraph-Isomorphic '''
