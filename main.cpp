d9969cf2760371c70d251d58f1afd41f695996e9
Kahsolt <kahsolt@qq.com>
2024-09-18 21:07:00 +0800

    migrate C impl.
    C impl. done, with debug codes

--- main.cpp
+++ main.cpp
@@ -57,22 +57,31 @@
 #pragma endregion
 
 #pragma region INCS_DEFS
+#include <cstdlib>
 #include <cstdio>
 #include <cstring>
 #include <ctime>
+#include <vector>
 #include <set>
 #include <map>
+#include <deque>
 
+using std::vector;
 using std::set;
 using std::map;
+using std::deque;
 
 #define IBUF   1000000  // input buffer
 #define OBUF   1000000  // output buffer
 #define MAX_M  50000    // test cases
-#define MAX_N  2000     // graph V
+#define MAX_N  2000     // graph V (1~2000)
 #define MAX_K  12000    // graph E
-#define MAX_Ni 50       // subgraph V
+#define MAX_Ni 50       // subgraph V (1~50)
 #define MAX_Ki 250      // subgraph E
+#define MAX_L  10       // label id (1~10)
+
+#define max(a, b) (a) > (b) ? (a) : (b)
+#define min(a, b) (a) < (b) ? (a) : (b)
 #pragma endregion
 
 #pragma region FAST_IO
@@ -102,83 +111,381 @@ struct Graph {
   int n;
   int k;
   int labels[MAX_N];
-  int degrees[MAX_N] = {0};
+  int degree[MAX_N] = {0};
   set<int> neighbors[MAX_N];
 };
 struct SubGraph {
   int n;
   int k;
   int labels[MAX_Ni];
-  int degrees[MAX_Ni];
-  set<int> neighbors[MAX_Ni];
+  int degree[MAX_Ni];
+  set<int> neighbors[MAX_Ni];   // TODO: use pure 2d-array?
+};
+struct Frame {
+  int u;           // cur_node
+  deque<int> nxt;  // candidates
 };
 // Info
 SubGraph G1; Graph G2;
-map<int, set<int>> nodes_of_G1Labels;
-map<int, set<int>> nodes_of_G2Labels;
-map<int, set<int>> G1_nodes_cover_degree;
-map<int, set<int>> G2_nodes_cover_degree;
+set<int> nodes_of_G1Labels[1 + MAX_L];
+set<int> nodes_of_G2Labels[1 + MAX_L];
+int label_rarity_G2[1 + MAX_L];
 // State
+Frame S[MAX_Ni]; int Sp;
+int node_order[MAX_Ni];
 int mapping[MAX_Ni];
 int reverse_mapping[MAX_N];
-set<int> T1;
-set<int> T2;
+bool T1[MAX_Ni];
+bool T2[MAX_N];
+set<int> u_labels_successors[1 + MAX_L];
+set<int> v_labels_successors[1 + MAX_L];
 // Result
 int ans[MAX_M][1 + MAX_Ni], n_ans = 0;  // prefixed by subgraph_idx
 
+void debug_node_order() {
+  printf("node_order: ");
+  for (int k = 0; k < G1.n; k++)
+    printf("%d ", node_order[k]);
+  printf("\n");
+}
+void debug_array(int x[], int len, char* name) {
+  printf("array %s: [", name);
+  for (int i = 0; i < len; i++)
+    printf("%d ", x[i]);
+  printf("]\n");
+}
+void debug_set(set<int> &s, char* name) {
+  printf("set %s: {", name);
+  for (auto it : s)
+    printf("%d ", it);
+  printf("}\n");
+}
+void debug_frame(Frame &frm) {
+  printf("frm: u=%d, nxt=[", frm.u);
+  for (auto it : frm.nxt)
+    printf("%d ", it);
+  printf("]\n");
+}
+
 inline void read_graph() {
-  int n, k, u, v;
+  int n, k, u, v, l;
   n = read(); G2.n = n;
   k = read(); G2.k = k;
-  for (int i = 0; i < n; i++)
-    G2.labels[i] = read();
+  for (int i = 0; i < n; i++) {
+    l = read();
+    G2.labels[i] = l;
+    nodes_of_G2Labels[l].insert(i);
+  }
   for (int i = 0; i < k; i++) {
-    u = read() - 1; G2.degrees[u]++;
-    v = read() - 1; G2.degrees[v]++;
+    u = read() - 1; G2.degree[u]++; // offset
+    v = read() - 1; G2.degree[v]++;
     G2.neighbors[u].insert(v);
     G2.neighbors[v].insert(u);
   }
+  // setup
+  for (int l = 1; l <= MAX_L; l++)
+    label_rarity_G2[l] = nodes_of_G2Labels[l].size();
 }
-void read_subgraph() {
-  int n, k, u, v;
+inline void read_subgraph() {
+  int n, k, u, v, l;
   n = read(); G1.n = n;
   k = read(); G1.k = k;
-  memset(G1.degrees, 0, sizeof(int) * n);  // reset
-  memset(mapping, 0x00, sizeof(mapping));
-  memset(reverse_mapping, 0x00, sizeof(reverse_mapping));
-  nodes_of_G2Labels.clear();
-  G2_nodes_cover_degree.clear();
-  T1.clear();
-  T2.clear();
+  for (int i = 1; i <= MAX_L; i++)
+    nodes_of_G1Labels[i].clear();
   for (int i = 0; i < n; i++) {
-    G1.labels[i] = read();
-    G1.neighbors[i].clear();               // reset
+    l = read();
+    G1.labels[i] = l;
+    nodes_of_G1Labels[l].insert(i);
+    G1.neighbors[i].clear();
   }
+  memset(G1.degree, 0x00, sizeof(int) * n);
   for (int i = 0; i < k; i++) {
-    u = read() - 1; G1.degrees[u]++;
-    v = read() - 1; G1.degrees[v]++;
+    u = read() - 1; G1.degree[u]++; // offset
+    v = read() - 1; G1.degree[v]++;
     G1.neighbors[u].insert(v);
     G1.neighbors[v].insert(u);
+  }
+  // reset
+  memset(mapping, 0xFF, sizeof(int) * n); // INVALID = -1
+  memset(reverse_mapping, 0xFF, sizeof(reverse_mapping)); // INVALID = -1
+  memset(T1, 0x00, sizeof(T1));
+  memset(T2, 0x00, sizeof(T2));
+}
+inline void write_ans() {
+  write(n_ans); putchar(10);
+  for (int j = 0; j < n_ans; j++) {
+    int* p = ans[j];
+    while (*p >= 0) {  // INVALID = -1
+      write(1 + *p++);  // inv-offset
+      putchar(32);
+    }
+    putchar(10);
   }
 }
 #pragma endregion
 
 #pragma region ALGORITHM
-void find_isomorphism(int i) {
-  n_ans = 2;
+inline void intersection_update(set<int> &a, set<int> &b) {
+  for (auto it = a.begin(); it != a.end();)
+    if (b.find(*it) == b.end()) { // not found
+      it = a.erase(it);
+    } else it++;
+}
+inline void difference_update(set<int> &s, int x[]) {
+  for (auto it = s.begin(); it != s.end();)
+    if (x[*it] >= 0) { // mapped
+      it = s.erase(it);
+    } else it++;
+}
+inline int count_exist(set<int> &s, bool flag[]) {
+  int cnt = 0;
+  for (auto e : s)
+    if (flag[e]) // found
+      cnt++;
+  return cnt;
+}
 
-  ans[0][0] = 2;
-  ans[0][1] = 2;
-  ans[0][2] = 4;
-  ans[0][3] = 3;
-  ans[0][4] = 1;
-  ans[0][5] = 0;  // terminal
-  
-  ans[1][0] = 4;
-  ans[1][1] = 1;
-  ans[1][2] = 3;
-  ans[1][3] = 2;
-  ans[1][4] = 0;  // terminal
+inline void _make_order() {
+  int label_rarity[1 + MAX_L];
+  memcpy(label_rarity, label_rarity_G2, sizeof(label_rarity_G2)); // copy!
+  int used_degree[MAX_Ni]; memset(used_degree, 0x00, sizeof(used_degree));
+  set<int> V1_unordered;
+  for (int i = 0; i < G1.n; i++)
+    V1_unordered.insert(i);
+  int p = 0;  // ptr(node_order)
+  int tmp;
+
+  while (!V1_unordered.empty()) {
+    int max_rarity = G1.n;
+    for (auto n : V1_unordered)
+      max_rarity = min(max_rarity, label_rarity[G1.labels[n]]);
+    printf("max_rarity: %d\n", max_rarity);
+    int sel_node = -1, max_deg = -1;
+    for (auto n : V1_unordered)
+      if (label_rarity[G1.labels[n]] == max_rarity) { // max rarity
+        tmp = G1.degree[n];
+        if (tmp > max_deg) {  // max deg
+          max_deg = tmp;
+          sel_node = n;
+        }
+      }
+    printf("sel_node: %d\n", sel_node);
+
+    bool visited[MAX_Ni]; memset(visited, 0x00, sizeof(visited));
+    visited[sel_node] = true;
+    set<int> current_layer = {sel_node};
+    set<int> next_layer;
+    set<int> nodes_to_add;
+    vector<int> max_used_degree_nodes;
+    vector<int> max_degree_nodes;
+    while (!current_layer.empty()) {
+      nodes_to_add.clear();
+      nodes_to_add = current_layer;   // copy!
+      while (!nodes_to_add.empty()) {
+        // max_used_degree
+        int max_used_degree = -1;
+        for (auto n : nodes_to_add)
+          max_used_degree = max(max_used_degree, used_degree[n]);
+        printf("max_used_degree: %d\n", max_used_degree);
+        max_used_degree_nodes.clear();
+        for (auto n : nodes_to_add)
+          if (used_degree[n] == max_used_degree)
+            max_used_degree_nodes.push_back(n);
+        // max_degree
+        int max_degree = -1;
+        for (auto n : max_used_degree_nodes)
+          max_degree = max(max_degree, G1.degree[n]);
+        printf("max_degree: %d\n", max_degree);
+        max_degree_nodes.clear();
+        for (auto n : max_used_degree_nodes)
+          if (G1.degree[n] == max_degree)
+            max_degree_nodes.push_back(n);
+        // max_rarity
+        int next_node = -1, min_cnt = G1.n;
+        for (auto n : max_degree_nodes) {
+          tmp = label_rarity[G1.labels[n]];
+          if (tmp < min_cnt) {
+            min_cnt = tmp;
+            next_node = n;
+          }
+        }
+        printf("next_node: %d\n", next_node);
+        // go!
+        nodes_to_add.erase(next_node);
+        V1_unordered.erase(next_node);
+        node_order[p++] = next_node;
+        label_rarity[G1.labels[next_node]]--;
+        for (auto n : G1.neighbors[next_node])
+          used_degree[n]++;
+      }
+
+      next_layer.clear();
+      for (auto n : current_layer)
+        for (auto e : G1.neighbors[n])
+          if (!visited[e]) {
+            visited[e] = true;
+            next_layer.insert(e);
+          }
+      current_layer = next_layer;
+    }
+  }
+}
+inline void _make_frame(int u) {
+  set<int> covered_neighbors;
+  for (auto nbr : G1.neighbors[u])
+    if (mapping[nbr] >= 0)   // mapped
+      covered_neighbors.insert(nbr);
+
+  set<int> &valid_label_nodes = nodes_of_G2Labels[G1.labels[u]];
+  set<int> valid_degree_nodes;
+  int D = G1.degree[u];
+  for (int i = 0; i <= G1.n; i++)
+    if (G2.degree[i] >= D)
+      valid_degree_nodes.insert(i);
+
+  debug_set(covered_neighbors, "covered_neighbors");
+  debug_set(valid_label_nodes, "valid_label_nodes");
+  debug_set(valid_degree_nodes, "valid_degree_nodes");
+
+  set<int> nxt;
+  if (covered_neighbors.empty()) {
+    nxt = valid_label_nodes;  // copy!
+    debug_set(nxt, "nxt");
+    intersection_update(nxt, valid_degree_nodes);
+    debug_set(nxt, "nxt");
+    difference_update(nxt, reverse_mapping);
+    debug_set(nxt, "nxt");
+  } else {
+    bool flag = true;
+    for (auto nbr : covered_neighbors) {
+      if (flag) {
+        nxt = G2.neighbors[mapping[nbr]];  // copy!
+        flag = false;
+      } else {
+        intersection_update(nxt, G2.neighbors[mapping[nbr]]);
+      }
+    }
+    debug_set(nxt, "nxt");
+    difference_update(nxt, reverse_mapping);
+    debug_set(nxt, "nxt");
+    intersection_update(nxt, valid_degree_nodes);
+    debug_set(nxt, "nxt");
+    intersection_update(nxt, valid_label_nodes);
+    debug_set(nxt, "nxt");
+  }
+  Frame &frm = S[Sp - 1];
+  frm.u = u;
+  frm.nxt.clear();
+  for (auto e : nxt)
+    frm.nxt.push_back(e);
+}
+inline bool _cut_PT(int u, int v) {
+  for (int l = 1; l <= MAX_L; l++) {
+    u_labels_successors[l].clear();
+    v_labels_successors[l].clear();
+  }
+  for (auto n1 : G1.neighbors[u])
+    u_labels_successors[G1.labels[n1]].insert(n1);
+  for (auto n2 : G2.neighbors[v])
+    v_labels_successors[G2.labels[n2]].insert(n2);
+
+  for (int l = 1; l <= MAX_L; l++) {
+    if (u_labels_successors[l].size() > v_labels_successors[l].size())
+      return true;
+    if (count_exist(u_labels_successors[l], T1) > count_exist(v_labels_successors[l], T2))
+      return true;
+  }
+  return false;
+}
+inline void _update_Tinout(int u, int v) {
+  for (auto succ : G1.neighbors[u])
+    if (mapping[succ] < 0) // not mapped
+      T1[succ] = true;
+  for (auto succ : G2.neighbors[v])
+    if (reverse_mapping[succ] < 0) // not mapped
+      T2[succ] = true;
+  T1[u] = false;
+  T2[v] = false;
+}
+inline void _restore_Tinout(int u, int v) {
+  bool found;
+  for (auto neighbor : G1.neighbors[u]) {
+    if (mapping[neighbor] >= 0) T1[u] = true;  // mapped
+    else {
+      found = false;
+      for (auto nbr : G1.neighbors[neighbor])
+        if (mapping[nbr] >= 0) { // mapped
+          found = true;
+          break;
+        }
+      if (!found) T1[neighbor] = false;
+    }
+  }
+  for (auto neighbor : G2.neighbors[v]) {
+    if (reverse_mapping[neighbor] >= 0) T2[v] = true;  // mapped
+    else {
+      found = false;
+      for (auto nbr : G2.neighbors[neighbor])
+        if (reverse_mapping[nbr] >= 0) { // mapped
+          found = true;
+          break;
+        }
+      if (!found) T2[neighbor] = false;
+    }
+  }
+}
+
+void find_isomorphism(int sgid) {
+  for (int l = 1; l <= MAX_L; l++)
+    if (nodes_of_G1Labels[l].size() > nodes_of_G2Labels[l].size())
+      return;
+
+  Sp = 0;  // ptr(node_order/stack), aka. dfs depth 
+  _make_order();
+  debug_node_order();
+
+  _make_frame(node_order[Sp++]);
+  while (Sp) {
+    Frame &frm = S[Sp - 1];
+    debug_frame(frm);
+
+    if (frm.nxt.empty()) { // no more candidates
+      Sp--;
+      if (Sp) {    // backtrace to last frame
+        Frame &frm = S[Sp - 1];
+        int u = frm.u;
+        int v = mapping[u];
+        mapping[u] = -1;
+        reverse_mapping[v] = -1;
+        _restore_Tinout(u, v);
+      }
+      continue;
+    }
+
+    int u = frm.u;
+    int v = frm.nxt.front(); frm.nxt.pop_front(); // try this new candidate
+    bool cut = _cut_PT(u, v);
+    printf("u->v: %d -> %d; cut=%d\n", u, v, cut);
+    if (!cut) {
+      if (Sp == G1.n) {
+        mapping[u] = v;
+        printf("u->v: %d -> %d", u, v);
+        int *q = ans[n_ans++];
+        *q++ = sgid;  // prefix with subgraph_id
+        printf("sgid: [%d]", sgid);
+        for (int i = 0; i < G1.n; i++)
+          *q++ = mapping[i];
+        *q = -1;      // mark terminal
+        debug_array(ans[n_ans-1], G1.n+2, "ans[n_ans]");
+        return;       // well done, exit!
+      } else {
+        mapping[u] = v;
+        reverse_mapping[v] = u;
+        _update_Tinout(u, v);
+        _make_frame(node_order[Sp++]);
+      }
+    }
+  }
 }
 #pragma endregion
 
@@ -186,24 +493,15 @@ int main() {
   // ttl control
   time_t ts = time(NULL); // sec
   time_t ttl = ts + 59;
-  // input & process
+  // process
   read_graph();
   int m = read();
-  for (int i = 1; i <= m; i++) {
+  for (int i = 0; i < m; i++) { // offset at output :)
     read_subgraph();
     find_isomorphism(i);
     if (time(NULL) >= ttl) break;
   }
-  // output
-  write(n_ans); putchar(10);
-  for (int j = 0; j < n_ans; j++) {
-    int* p = ans[j];
-    while (*p) {  // zero-terminated
-      write(*p++);
-      putchar(32);
-    }
-    putchar(10);
-  }
+  write_ans();
   // clear buff
   fwrite(obuf, p3 - obuf, 1, stdout);
   return 0;
